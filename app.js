1d8cc06c70be88921fc1c8da54884f456b91a1b9
Juno Han <junhohan2@gmail.com>
2021-09-16 03:37:35 -0700

    fixed minor logic issues

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 const clear = document.querySelector(".clear");
-const pixel = document.querySelector(".pixel");
+const container = document.querySelector(".container");
 
-function clean() {
+function deletePixel() {
+  squares.forEach((square) => square.remove());
+}
+
+function makeBoard(pixel) {
+  let totalPixels = pixel * pixel;
+  for (let i = 0; i < totalPixels; i++) {
+    let newPixel = document.createElement("div");
+    newPixel.classList.add("square");
+    container.appendChild(newPixel);
+  }
+}
+
+function askUser() {
+  let pixelSize = +window.prompt("Enter grid size between 1 - 64", 64);
+  if (pixelSize > 64 || pixelSize < 0) {
+    return;
+  }
+  deletePixel();
+  makeBoard(pixelSize * pixelSize);
+  container.style.gridTemplate = `repeat(${pixelSize}, 1fr) / repeat(${pixelSize}, 1fr)`;
+  squares = document.querySelectorAll(".square");
+  squares.forEach((square) =>
+    square.addEventListener("mouseover", changeColor)
+  );
+}
+
+function reset() {
+  askUser();
   squares.forEach((square) => (square.style.backgroundColor = "white"));
 }
 
@@ -18,6 +46,6 @@ function init() {
   squares.forEach((square) =>
     square.addEventListener("mouseover", changeColor)
   );
-  clear.addEventListener("click", clean);
+  clear.addEventListener("click", reset);
 }
 init();
